9ebc661536da7c27de1e3bcc4cdbe49fd8c108df
Dan <baghdadi.danny@gmail.com>
2023-01-31 20:48:40 -0800

    font update + font preloader

--- client/src/scenes/internal/Load.ts
+++ client/src/scenes/internal/Load.ts
@@ -1,4 +1,5 @@
 import Phaser from "phaser";
+import WebFont from "webfontloader";
 
 //
 // This is meant for loading in any data, such as a save game state, prior to entering the main menu.
@@ -9,9 +10,16 @@ export class Load extends Phaser.Scene {
 		super({ key: "Load" });
 	}
 
-	preload() {}
-
-	create() {
-		this.scene.start("Menu");
+	preload() {
+		WebFont.load({
+			active: () => {
+				// fonts are loaded, continue to next scene
+				this.scene.start("Menu");
+			},
+			// custom fonts
+			custom: {
+				families: ["Pix"]
+			},
+		});
 	}
 }
