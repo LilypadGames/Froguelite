fee2165c8ba9278f0b4260d7efa825f3c77e13c3
Dan <baghdadi.danny@gmail.com>
2023-02-01 00:14:59 -0800

    tilemap implementation, camera following player, velocity dampening dependent on direction

--- client/src/scenes/Game.ts
+++ client/src/scenes/Game.ts
@@ -16,7 +16,8 @@ export class Game extends Core {
 	};
 
 	// config
-	velocity: number = 300;
+	velocity: number = 30;
+	velocityDampening: number = 1.8;
 	turnThreshold: number = 150;
 
 	constructor() {
@@ -26,6 +27,10 @@ export class Game extends Core {
 	preload() {
 		// preload core mechanics
 		this.core.preload();
+
+		// load world
+		this.load.image("world_tiles", "assets/world/tiles.png");
+		this.load.tilemapTiledJSON("riverside", "assets/world/riverside.json");
 
 		// player character sprite sheet
 		this.load.spritesheet("pp", "assets/character/pp.png", {
@@ -48,14 +53,43 @@ export class Game extends Core {
 			D: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
 		};
 
+		// create world
+		const map = this.make.tilemap({ key: "riverside" });
+		const tileset = map.addTilesetImage("tiles", "world_tiles");
+		map.createLayer(
+			"Grass",
+			tileset,
+			window.innerWidth / 2,
+			window.innerHeight / 2
+		);
+		map.createLayer(
+			"Water",
+			tileset,
+			window.innerWidth / 2,
+			window.innerHeight / 2
+		);
+
 		// add player to scene
 		this.player = this.addPlayer(
 			window.innerWidth / 2,
 			window.innerHeight / 2
 		);
+
+		// set up camera
+		this.cameras.main.startFollow(this.player, false, 1, 1, 0, 0);
+		this.cameras.main.setZoom(8);
+
+		// fade in to begin
+		// this.cameras.main.fadeIn();
 	}
 
 	update() {
+		// init direction
+		let direction: string = "";
+
+		// init velocity
+		let velocity: number = this.velocity;
+
 		// if pointer down, make player face the pointer
 		if (this.input.activePointer.isDown) {
 			let mouse = {
@@ -71,18 +105,22 @@ export class Game extends Core {
 			// player looking left
 			if (difference.x > this.turnThreshold) {
 				this.player.anims.play("left", true);
+				direction = "left";
 			}
 			// player looking right
 			else if (difference.x < -this.turnThreshold) {
 				this.player.anims.play("right", true);
+				direction = "right";
 			}
 			// player looking away from the player
 			else if (difference.y > 0) {
 				this.player.anims.play("back", true);
+				direction = "up";
 			}
 			// player looking towards the player
 			else if (difference.y <= 0) {
 				this.player.anims.play("front", true);
+				direction = "down";
 			}
 		}
 
@@ -112,8 +150,15 @@ export class Game extends Core {
 
 		// moving left
 		if (key.left.isDown) {
+			// determine direction
+			if (direction == "") direction = "left";
+
+			// determine velocity
+			if (direction !== "left")
+				velocity = this.velocity / this.velocityDampening;
+
 			// move left
-			this.player.setVelocityX(-this.velocity);
+			this.player.setVelocityX(-velocity);
 
 			// play moving left animation
 			if (!this.input.activePointer.isDown)
@@ -122,8 +167,15 @@ export class Game extends Core {
 
 		// moving right
 		if (key.right.isDown) {
+			// determine direction
+			if (direction === "") direction = "right";
+
+			// determine velocity
+			if (direction !== "right")
+				velocity = this.velocity / this.velocityDampening;
+
 			// move right
-			this.player.setVelocityX(this.velocity);
+			this.player.setVelocityX(velocity);
 
 			// play moving right animation
 			if (!this.input.activePointer.isDown)
@@ -132,8 +184,15 @@ export class Game extends Core {
 
 		// moving up
 		if (key.up.isDown) {
+			// determine direction
+			if (direction === "") direction = "up";
+
+			// determine velocity
+			if (direction !== "up")
+				velocity = this.velocity / this.velocityDampening;
+
 			// move up
-			this.player.setVelocityY(-this.velocity);
+			this.player.setVelocityY(-velocity);
 
 			// play moving up animation
 			if (!this.input.activePointer.isDown)
@@ -142,8 +201,15 @@ export class Game extends Core {
 
 		// moving down
 		if (key.down.isDown) {
+			// determine direction
+			if (direction === "") direction = "down";
+
+			// determine velocity
+			if (direction !== "down")
+				velocity = this.velocity / this.velocityDampening;
+
 			// move down
-			this.player.setVelocityY(this.velocity);
+			this.player.setVelocityY(velocity);
 
 			// play moving down animation
 			if (!this.input.activePointer.isDown)
@@ -163,10 +229,7 @@ export class Game extends Core {
 
 	addPlayer(x: number, y: number) {
 		// create player
-		let player = this.physics.add
-			.sprite(x, y, "pp")
-			.setScale(10)
-			.setOrigin(0.5, 0.5);
+		let player = this.physics.add.sprite(x, y, "pp").setOrigin(0.5, 0.5);
 
 		// make player collide with world bounds
 		player.setCollideWorldBounds(true);
