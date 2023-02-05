c6ff3bd88fc72f14dccce3df6129d03d7b4fc133
Dan <baghdadi.danny@gmail.com>
2023-02-04 16:22:09 -0800

    show/hide main menu during pause

--- client/src/scenes/Menu.ts
+++ client/src/scenes/Menu.ts
@@ -5,6 +5,9 @@ import { Core } from "./internal/Core";
 //
 
 export class Menu extends Core {
+	logo!: Phaser.GameObjects.Text;
+	begin!: Phaser.GameObjects.Text;
+
 	constructor() {
 		super({ key: "Menu" });
 	}
@@ -25,6 +28,9 @@ export class Menu extends Core {
 
 		// options menu
 		this.keyESC.on("down", () => {
+			// hide current scene
+			this.hide();
+
 			// pause current scene
 			this.scene.pause();
 
@@ -33,7 +39,7 @@ export class Menu extends Core {
 		});
 
 		// logo text
-		const logo = this.make.text({
+		this.logo = this.make.text({
 			x: window.innerWidth / 2,
 			y: window.innerHeight / 3,
 			text: "Froguelike",
@@ -50,7 +56,7 @@ export class Menu extends Core {
 		});
 
 		// begin text
-		const begin = this.make.text({
+		this.begin = this.make.text({
 			x: window.innerWidth / 2,
 			y: (window.innerHeight / 5) * 4,
 			text: "Click Anywhere To Begin",
@@ -66,7 +72,7 @@ export class Menu extends Core {
 
 		// begin text animation
 		this.tweens.add({
-			targets: begin,
+			targets: this.begin,
 			scaleX: 1.15,
 			scaleY: 1.15,
 			duration: 800,
@@ -79,5 +85,18 @@ export class Menu extends Core {
 		this.input.on("pointerdown", () => {
 			this.changeScene("Game");
 		});
+
+		// show menu when resumed
+		this.events.on("resume", this.show, this);
+	}
+
+	show() {
+		this.logo.setVisible(true);
+		this.begin.setVisible(true);
+	}
+
+	hide() {
+		this.logo.setVisible(false);
+		this.begin.setVisible(false);
 	}
 }
