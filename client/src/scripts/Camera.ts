8dbb387567cbff6a2954fe00092f72f26b29316a
Dan <danmizu@nostalgi.cc>
2023-05-06 02:20:33 -0700

    store camera rotation

--- client/src/scripts/Camera.ts
+++ client/src/scripts/Camera.ts
@@ -11,6 +11,7 @@ export class Camera extends Phaser.Cameras.Scene2D.Camera {
 
 	// config
 	rotationSpeed: number = 0.03;
+	rotationDefault: number = 0;
 	zoomInterval: number = 1;
 	zoomMax: number = 18;
 	zoomMin: number = 5;
@@ -29,9 +30,16 @@ export class Camera extends Phaser.Cameras.Scene2D.Camera {
 
 		// set camera zoom
 		this.setZoom(
-			store.get("settings.options.zoom")
-				? store.get("settings.options.zoom")
+			store.get("settings.options.camera.zoom")
+				? store.get("settings.options.camera.zoom")
 				: this.zoomDefault
+		);
+
+		// set camera rotation
+		this.updateRotation(
+			store.get("settings.options.camera.rotation")
+				? store.get("settings.options.camera.rotation")
+				: this.rotationDefault
 		);
 
 		// populate key inputs
@@ -57,7 +65,7 @@ export class Camera extends Phaser.Cameras.Scene2D.Camera {
 
 	update() {
 		// rotate camera
-		this.handleRotation();
+		if (this.keyQE.Q.isDown || this.keyQE.E.isDown) this.handleRotation();
 	}
 
 	resize(gameSize: Phaser.Structs.Size) {
@@ -77,35 +85,34 @@ export class Camera extends Phaser.Cameras.Scene2D.Camera {
 		// min
 		else if (this.zoom < this.zoomMin) this.zoom = this.zoomMin;
 
-		// store
-		store.set("settings.options.zoom", this.zoom);
+		// store zoom
+		store.set("settings.options.camera.zoom", this.zoom);
 	}
 
 	// handle rotation of objects that are to be fixed to camera rotation
 	handleRotation() {
-		// rotate left
-		if (this.keyQE.Q.isDown) {
-			// rotate cam
-			(this as any).rotation += this.rotationSpeed;
+		// get new rotation
+		let newRotation = this.keyQE.Q.isDown
+			? (this as any).rotation + this.rotationSpeed
+			: (this as any).rotation - this.rotationSpeed;
 
-			// rotate objects counter to cameras rotation
-			this.scene.fixedObjectsGroup
-				.getChildren()
-				.forEach((object: GameObjects.GameObject) => {
-					(object as any).rotation = -(this as any).rotation;
-				}, this);
-		}
-		// rotate right
-		else if (this.keyQE.E.isDown) {
-			// rotate cam
-			(this as any).rotation -= this.rotationSpeed;
+		// store rotation
+		store.set("settings.options.camera.rotation", newRotation);
 
-			// rotate objects counter to cameras rotation
-			this.scene.fixedObjectsGroup
-				.getChildren()
-				.forEach((object: GameObjects.GameObject) => {
-					(object as any).rotation = -(this as any).rotation;
-				}, this);
-		}
+		// update rotation
+		this.updateRotation(newRotation);
+	}
+
+	// updates current camera rotation as well as objects in scene
+	updateRotation(rotation: number) {
+		// set rotation
+		(this as any).rotation = rotation;
+
+		// rotate objects counter to cameras rotation
+		this.scene.fixedObjectsGroup
+			.getChildren()
+			.forEach((object: GameObjects.GameObject) => {
+				(object as any).rotation = -(this as any).rotation;
+			}, this);
 	}
 }
