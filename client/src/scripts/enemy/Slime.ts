b1fce80e09aca78fe4b5c55960959c6ff1052908
Dan <danmizu@nostalgi.cc>
2023-05-14 00:19:13 -0700

    created slime class

---
+++ client/src/scripts/enemy/Slime.ts
@@ -0,0 +1,21 @@
+// scenes
+import { Game } from "../../scenes/Game";
+
+// components
+import { Enemy } from "../Enemy";
+
+export class Slime extends Enemy {
+	pounceTime = {
+		min: 2000,
+		max: 5000,
+	};
+
+	constructor(scene: Game, x: number, y: number, id: string) {
+		// pass values
+		super(scene, x, y, id);
+	}
+
+	update() {}
+
+	handleAI() {}
+}
