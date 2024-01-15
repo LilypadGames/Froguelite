c3dc0cf207f65905c24f77c96a4c9643e9e0a22d
Dan <dan@danmizu.dev>
2024-01-15 13:51:15 -0800

    feat: use lang file for title screen

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,10 +1,21 @@
 extends CanvasLayer
 
 # references
+@export_category("References")
+@onready var title: Label = %Title
+@onready var hint: Label = %Hint
 @export var level: PackedScene
 
+# set up labels
+func _ready() -> void:
+	# title
+	title.text = Cache.lang["en_us"]["game_title"]
+
+	# hint
+	hint.text = Cache.lang["en_us"]["opening_hint"]
+
 # detect any input
-func _input(event):
+func _input(event) -> void:
 	if event.is_pressed():
 		# start game
 		get_tree().change_scene_to_packed(level)
