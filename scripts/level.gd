b6824b5ec985d8074e982c18dce67e5ae3721181
Dan <dan@danmizu.dev>
2024-02-11 16:55:17 -0800

    feat: decoration layer

--- scripts/level.gd
+++ scripts/level.gd
@@ -16,7 +16,7 @@ extends Node2D
 @export var chunk_distance: int = 2
 var level_seed: int
 var infinite_tilemap: TileMap
-var tileset_tiles: Dictionary = {"texture": {}, "weight": {}, "weight_total": {}, "source_id": {}}
+var tileset_tiles: Dictionary = {"texture": {}, "weight": {}, "weight_padding": {}, "weight_total": {}, "source_id": {}}
 var tile_size: int
 var loaded_chunks: Array[Vector2i] = []
 
@@ -150,6 +150,7 @@ func _init_procedural_level() -> void:
 	# create tilemap
 	infinite_tilemap = TileMap.new()
 	infinite_tilemap.rendering_quadrant_size = tile_size
+	infinite_tilemap.add_layer(-1)
 	level.add_child(infinite_tilemap)
 
 	# create tileset
@@ -159,10 +160,12 @@ func _init_procedural_level() -> void:
 
 	# get tiles
 	var tile_data: Dictionary = Cache.registry["world"]["level"][level_name]["tiles"]
+	var source_count: int = 0
 	for tile in tile_data:
-		# create arrays
+		# init tile type properties
 		tileset_tiles["texture"][tile] = [] as Array[Texture2D]
 		tileset_tiles["weight"][tile] = [] as Array[int]
+		tileset_tiles["weight_padding"][tile] = 0 as int
 		tileset_tiles["weight_total"][tile] = [] as Array[int]
 		tileset_tiles["source_id"][tile] = [] as Array[int]
 
@@ -172,21 +175,34 @@ func _init_procedural_level() -> void:
 		# add texture data to tileset_tiles dict
 		var weight_total: int = 0
 		for texture_path in textures.keys():
-			tileset_tiles["texture"][tile].push_back(load(texture_path))
-			tileset_tiles["weight"][tile].push_back(textures[texture_path] as int)
+			# weight padding
+			if texture_path == "*":
+				tileset_tiles["weight_padding"][tile] = textures[texture_path] as int
+
+			# add tile texture and weight
+			else:
+				tileset_tiles["texture"][tile].push_back(load(texture_path))
+				tileset_tiles["weight"][tile].push_back(textures[texture_path] as int)
+
+			# keep track of total weight
 			weight_total += textures[texture_path] as int
+
+		# save total weight
 		tileset_tiles["weight_total"][tile] = weight_total
 
 		# create tiles and add to tile set
 		for texture_index in tileset_tiles["texture"][tile].size():
 			var atlas_source = TileSetAtlasSource.new()
-			atlas_source.texture = tileset_tiles["texture"]["ground"][texture_index]
+			atlas_source.texture = tileset_tiles["texture"][tile][texture_index]
 			atlas_source.texture_region_size = Vector2i(tile_size, tile_size)
 			atlas_source.create_tile(Vector2i(0, 0))
-			tile_set.add_source(atlas_source, texture_index)
+			tile_set.add_source(atlas_source, source_count)
 
 			# save source ID
-			tileset_tiles["source_id"][tile].push_back(texture_index)
+			tileset_tiles["source_id"][tile].push_back(source_count)
+
+			# next source ID
+			source_count += 1
 
 func _handle_procedural_level() -> void:
 	# get player's current chunk
@@ -205,46 +221,16 @@ func _handle_procedural_level() -> void:
 				_load_chunk(Vector2i(x, y))
 				loaded_chunks.push_back(Vector2i(x, y))
 
-func _get_player_chunk_position() -> Vector2i:
-	return Vector2i(floor((player.global_position.x / tile_size) / chunk_size), floor((player.global_position.y / tile_size) / chunk_size))
-
 func _load_chunk(chunk_position: Vector2i) -> void:
 	# set tile RNG to chunk's seed
 	var tile_rng = RandomNumberGenerator.new()
 	tile_rng.seed = _get_chunk_seed(chunk_position)
 
-	# add tiles
-	for x in range(chunk_size):
-		for y in range(chunk_size):
-			# tile's world position
-			var tile_position = _get_tile_tilemap_position(chunk_position, Vector2i(x, y))
+	# ground tiles
+	_generate_tiles(chunk_position, "ground", 0, tile_rng)
 
-			# is there a tile at this noise position?
-			if floor(abs(noise.get_noise_2d(tile_position.x, tile_position.y) * 2)) == 0:
-				# determine tile to use
-				var tile_id := 0
-				var rand: int = tile_rng.randi_range(1, tileset_tiles["weight_total"]["ground"])
-				var weights: Array[int] = tileset_tiles["weight"]["ground"]
-				if weights.size() > 1:
-					# keep track of accumulating weight
-					var weight_sum := 0
-
-					# compare weight to random value
-					for weight_index in weights.size():
-						weight_sum += weights[weight_index]
-
-						# last tile
-						if weight_index == weights.size() - 1:
-							tile_id = tileset_tiles["source_id"]["ground"][weight_index]
-							break
-
-						# passed the correct tile
-						elif rand <= weight_sum:
-							tile_id = tileset_tiles["source_id"]["ground"][weight_index]
-							break
-
-				# add tile
-				infinite_tilemap.set_cell(0, Vector2i(tile_position.x, tile_position.y), tile_id, Vector2i(0, 0))
+	# decoration tiles
+	_generate_tiles(chunk_position, "decoration", 1, tile_rng)
 
 func _unload_chunk(chunk_position: Vector2i) -> void:
 	# remove tiles
@@ -256,8 +242,53 @@ func _unload_chunk(chunk_position: Vector2i) -> void:
 			# remove tile
 			infinite_tilemap.erase_cell(0, Vector2i(tile_position.x, tile_position.y))
 
-func _get_tile_tilemap_position(chunk_position: Vector2i, tile_index: Vector2i) -> Vector2i:
-	return chunk_position * chunk_size + tile_index
+func _generate_tiles(chunk_position: Vector2i, tile_type: String, layer: int, tile_rng: RandomNumberGenerator) -> void:
+	# add tiles
+	for x in range(chunk_size):
+		for y in range(chunk_size):
+			# tile's world position
+			var tile_position = _get_tile_tilemap_position(chunk_position, Vector2i(x, y))
+
+			# is there a tile at this noise position?
+			if _is_filled_at_noise_position(tile_position):
+				# init tile id to use
+				var tile_id: int
+
+				# get random number based on weight total
+				var rand: int = tile_rng.randi_range(1, tileset_tiles["weight_total"][tile_type])
+
+				# keep track of accumulating weight
+				var weight_sum: int = tileset_tiles["weight_padding"][tile_type]
+
+				# random number is not in padding
+				if not rand <= weight_sum:
+					# get weight list
+					var weights: Array[int] = tileset_tiles["weight"][tile_type]
+
+					# compare weight to random value
+					for weight_index in weights.size():
+						# add weight
+						weight_sum += weights[weight_index]
+
+						# passed the correct tile
+						if rand <= weight_sum:
+							tile_id = tileset_tiles["source_id"][tile_type][weight_index]
+							break
+
+						# end of iteration
+						tile_id = tileset_tiles["source_id"][tile_type][weight_index]
+
+					# add tile
+					infinite_tilemap.set_cell(layer, Vector2i(tile_position.x, tile_position.y), tile_id, Vector2i(0, 0))
 
 func _get_chunk_seed(chunk_position: Vector2i) -> int:
 	return (chunk_position.x * 73856093) ^ (chunk_position.y * 19349663) ^ level_seed
+
+func _get_player_chunk_position() -> Vector2i:
+	return Vector2i(floor((player.global_position.x / tile_size) / chunk_size), floor((player.global_position.y / tile_size) / chunk_size))
+
+func _get_tile_tilemap_position(chunk_position: Vector2i, tile_index: Vector2i) -> Vector2i:
+	return chunk_position * chunk_size + tile_index
+
+func _is_filled_at_noise_position(tile_position: Vector2i) -> bool:
+	return floor(abs(noise.get_noise_2d(tile_position.x, tile_position.y) * 2)) == 0
